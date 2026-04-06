d798f187f18f77a5a4fd0ba97d6c9df3ab6c5da4
Yao Zhou <your@email.com>
2026-04-06 21:04:09 +0200

    debug: bypass ingest proxy, use direct posthog endpoint with debug on

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,9 +1,9 @@
 import posthog from "posthog-js";
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/ingest",
+  api_host: "https://eu.i.posthog.com",
   ui_host: "https://eu.posthog.com",
   defaults: "2026-01-30",
   capture_exceptions: true,
-  debug: process.env.NODE_ENV === "development",
+  debug: true,
 });
